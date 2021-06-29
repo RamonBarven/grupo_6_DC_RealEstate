c2f957093d81ae49b15d2bdba1c5510b046ab0f4
maxcserrano <maxiroserrano@gmail.com>
2021-06-29 16:07:23 -0500

    se agregó el headeren html y css

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express=require ('express');
 const path=require('path');
 
 const app=express();
+app.use(express.static('public'));
+
 app.listen(3000,()=>{
     console.log('Servidor corriendo en el puerto 3000');
 });
