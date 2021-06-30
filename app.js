fa99c54b051f1f667ef0d98a820fccf4b0fd2684
Arlette3097 <arlette.sanchez@upaep.edu.mx>
2021-06-30 15:07:18 -0500

    Agregue página Favoritos

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ app.listen(3000,()=>{
     console.log('Servidor corriendo en el puerto 3000');
 });
 
-app.get('/', (req,res) => {
+/*app.get('/', (req,res) => {
     let htmlPath = path.resolve(__dirname,'./views/index.html');
+    res.sendFile(htmlPath);
+} )
+*/
+app.get('/', (req,res) => {
+    let htmlPath = path.resolve(__dirname,'./views/favoritos.html');
     res.sendFile(htmlPath);
 } )
