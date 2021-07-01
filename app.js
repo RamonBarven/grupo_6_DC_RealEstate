0d53e5821b82b23ccadcec9ea94a712dc1c0aafa
Arlette3097 <arlette.sanchez@upaep.edu.mx>
2021-07-01 17:06:36 -0500

    modificación favoritos y sign up

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.listen(3000,()=>{
     console.log('Servidor corriendo en el puerto 3000');
 });
 
-/*app.get('/', (req,res) => {
+app.get('/', (req,res) => {
     let htmlPath = path.resolve(__dirname,'./views/index.html');
     res.sendFile(htmlPath);
 } )
@@ -21,9 +21,9 @@ app.get('/', (req,res) => {
 app.get('/', (req,res) => {
     let htmlPath = path.resolve(__dirname,'./views/favoritos.html');
     res.sendFile(htmlPath);
-} )*/
+} )
 
 app.get('/', (req,res) => {
     let htmlPath = path.resolve(__dirname,'./views/signUp.html');
     res.sendFile(htmlPath);
-} )
+} ) 
