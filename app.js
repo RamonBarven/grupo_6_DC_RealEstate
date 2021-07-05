5d1784a96f3ce501a0706e2101ca1ccf0f3be2ac
Arlette3097 <arlette.sanchez@upaep.edu.mx>
2021-07-05 17:25:48 -0500

    Se agregó la parte responsiva de sign up

--- app.js
+++ app.js
@@ -8,22 +8,27 @@ app.listen(3000,()=>{
     console.log('Servidor corriendo en el puerto 3000');
 });
 
-app.get('/', (req,res) => {
+app.get('/home', (req,res) => {
     let htmlPath = path.resolve(__dirname,'./views/index.html');
     res.sendFile(htmlPath);
 } )
 
-app.get('/', (req,res) => {
+app.get('/login', (req,res) => {
     let htmlPath = path.resolve(__dirname,'./views/login.html');
     res.sendFile(htmlPath);
 } )
 
-app.get('/', (req,res) => {
+app.get('/favorites', (req,res) => {
     let htmlPath = path.resolve(__dirname,'./views/favoritos.html');
     res.sendFile(htmlPath);
 } )
 
-app.get('/', (req,res) => {
+app.get('/signup', (req,res) => {
     let htmlPath = path.resolve(__dirname,'./views/signUp.html');
+    res.sendFile(htmlPath);
+} ) 
+
+app.get('/productdetail', (req,res) => {
+    let htmlPath = path.resolve(__dirname,'./views/detalleProducto.html');
     res.sendFile(htmlPath);
 } ) 
