5c29c24364db62b22e6ff52e12c5c23f89925688
maxcserrano <maxiroserrano@gmail.com>
2021-07-23 21:09:36 -0500

    corrección de routes

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,8 +2,9 @@ let express=require('express');
 let router= express.Router();
 let productController=require("../controllers/products/productController");
 
-router.get('/:id', productController.detail);
-router.get('/', productController.list);
 router.get('/add', productController.add);
 router.post('/add', productController.addpost);
+router.get('/', productController.list);
+router.get('/:id', productController.detail);
+
 module.exports=router;
