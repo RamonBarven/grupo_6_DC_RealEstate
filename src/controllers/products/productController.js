c4406d3aa4bb8078112888f9c91f4abf066791e8
RamonBarven <r.barraza1509@gmail.com>
2021-07-23 20:17:55 -0500

    Intento fallido de generar varias vistas de detail

--- src/controllers/products/productController.js
+++ src/controllers/products/productController.js
@@ -4,15 +4,20 @@ const fs=require("fs");
 
 let productController={
     detail:function (req,res) {
-        res.render('product/detail')},
+        let id = req.params.id-1;
+        let datos=fs.readFileSync("data/product.json","utf-8");
+        let datosjson=JSON.parse(datos);
+        res.render('product/detail', {datosjson: datosjson, id: id}); 
+    },
     add:function (req,res) {
         res.render('product/addProduct')},
     list:function(req,res){
         res.render('product/listProduct')},
     addpost:function(req,res){
         let productos={         
-        id:7,
-        saleRent:req.body.saleRent,
+        id: Date.now(),
+        image:req.body.image,
+        category:req.body.category,
         price:req.body.price,
         description:req.body.description,
         location:req.body.location,
--- src/routes/products.js
+++ src/routes/products.js
@@ -2,7 +2,7 @@ let express=require('express');
 let router= express.Router();
 let productController=require("../controllers/products/productController");
 
-router.get('/detail', productController.detail);
+router.get('/:id', productController.detail);
 router.get('/', productController.list);
 router.get('/add', productController.add);
 router.post('/add', productController.addpost);
